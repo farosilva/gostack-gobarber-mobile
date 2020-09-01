761901ca70bc04c45ffcf43a27beb442ddf89752
Fabricio Silva <fabricio.roddrigo@gmail.com>
2020-08-31 21:59:36 -0300

    Sign in design concluded

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Image, KeyboardAvoidingView, Platform, View, Keyboard, ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logoImg} />
+const SignIn: React.FC = () => {
+  const [isVisibleCreateAccountButton, setIsVisibleCreateAccountButton] = useState(true);
 
-    <Title>Faça seu logon</Title>
+  useEffect(() => {
+    const keyboardShow = Keyboard.addListener('keyboardDidShow',
+      () => setIsVisibleCreateAccountButton(false));
 
-    <Input name="email" icon="mail" placeholder="E-mail" />
-    <Input name="password" icon="lock" placeholder="Senha" />
+    const keyboardHide = Keyboard.addListener('keyboardDidHide',
+      () => setIsVisibleCreateAccountButton(true));
 
-    <Button onPress={() => { console.log('OK'); }}>Entrar</Button>
-  </Container>
-);
+    return () => {
+      keyboardShow.remove();
+      keyboardHide.remove();
+    };
+  }, []);
+
+  return (
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      contentContainerStyle={{ flex: 1 }}
+    >
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <Container>
+          <Image source={logoImg} />
+
+          {/* View - Permitir animação */}
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button
+            onPress={() => { console.log(''); }}
+          >
+            Entrar
+          </Button>
+
+          <ForgotPassword onPress={() => { console.log(''); }}>
+            <ForgotPasswordText>Esqueceu sua senha?</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </KeyboardAvoidingView>
+
+      {isVisibleCreateAccountButton && (
+        <CreateAccountButton onPress={() => { console.log(''); }}>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>
+            Criar uma conta
+          </CreateAccountButtonText>
+        </CreateAccountButton>
+      )}
+    </ScrollView>
+  );
+};
 
 export default SignIn;
